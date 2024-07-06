f8f89e411f4f951844f36ad71ffeca67fbe405fb
NeiJunio <neijunio50@gmail.com>
2024-07-05 23:29:31 -0300

    implementação inicial do modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,50 +1,71 @@
 const pokemonList = document.getElementById('pokemonList');
 const loadMoreButton = document.getElementById('loadMoreButton');
-
-const maxRecords = 151 //Limitar somente na 1ª geração dos pokemons
-const limit = 10
-let offset = 0;
-
+const modalContent = document.querySelector('.modal-content');
+const modal = document.querySelector('.modal');
+const closeButton = document.querySelector('.close');
 
 
-function loadPokemonItens(offset, limit) {
-    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map((pokemon) =>
-            `<li class="pokemon ${pokemon.type}">
-                <span class="number">#${pokemon.number} </span>
+function openModal(pokemon) {
+    const numberPokemon = document.getElementById('number-pokemon');
+    const name = modal.querySelector('.name');
+    const types = modal.querySelector('.types');
+    // const modalImage = modal.querySelector('.modal-header img');
     
-                <span class="name">${pokemon.name}</span>
-                
-                <div class="detail">
-                    <ol class="types">
-                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                    </ol>
-
-                    <img src="${pokemon.photo}"
-                        alt="${pokemon.name}">
-                </div>
-            </li>
-        `).join('')
-
-        pokemonList.innerHTML += newHtml
-    })
+    numberPokemon.textContent = `#${pokemon.number}`;
+    name.textContent = pokemon.name;
+    types.innerHTML = pokemon.types.map(type => `<li class="type ${type}">${type}</li>`).join('');
+    // modalImage.src = pokemon.photo;
+    // modalImage.alt = pokemon.name;
+    
+    modalContent.classList.remove('hidden');
 }
 
-loadPokemonItens(offset, limit)
+function closeModal() {
+    modalContent.classList.add('hidden');
+}
+
+const maxRecords = 151; // Limitar somente na 1ª geração dos pokemons
+const limit = 10;
+let offset = 0;
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => 
+            `<li class="pokemon ${pokemon.type}" data-number="${pokemon.number}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map(type => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">
+                </div>
+            </li>`
+        ).join('');
+
+        pokemonList.innerHTML += newHtml;
+
+        const pokemonElements = pokemonList.querySelectorAll('.pokemon');
+        pokemonElements.forEach((element, index) => {
+            element.addEventListener('click', () => openModal(pokemons[index]));
+        });
+    });
+}
+
+loadPokemonItens(offset, limit);
 
 loadMoreButton.addEventListener('click', () => {
-    offset += limit
+    offset += limit;
 
-    const qtRecordNexPage = offset + limit
+    const qtRecordNexPage = offset + limit;
 
     if (qtRecordNexPage >= maxRecords) {
-        const newLimit = maxRecords - offset
-        loadPokemonItens(offset, newLimit)
+        const newLimit = maxRecords - offset;
+        loadPokemonItens(offset, newLimit);
 
-        loadMoreButton.parentElement.removeChild(loadMoreButton)
+        loadMoreButton.parentElement.removeChild(loadMoreButton);
     } else {
-        loadPokemonItens(offset, limit)
+        loadPokemonItens(offset, limit);
     }
+});
 
-
-})
+closeButton.addEventListener('click', closeModal);
